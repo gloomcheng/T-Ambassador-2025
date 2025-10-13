11e718ac973e4dfa667a4900ec1ac08666fb2fa4
Felix <s20316117@Gmail.com>
2025-10-13 14:57:34 +0800

    重建虛擬環境
    pyvenv.cfg 及 Scripts 目錄：重建虛擬環境，Python 版本升級，啟動腳本路徑與提示修正。
    config.yml：新增空白設定檔，預留未來設定使用。

--- exhibition/backend/benjamin/Scripts/activate.bat
+++ exhibition/backend/benjamin/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\workspace\tdance2024\benjamin
+set "VIRTUAL_ENV=D:\T-Ambassador-2025\exhibition\backend\benjamin"
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(benjamin) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(benjamin) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(benjamin) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=benjamin"
 
 :END
 if defined _OLD_CODEPAGE (
