b743d5da981850904178bd8dc854d91369ff18f0
Fuyuan Cheng <gloomcheng@zhelang.com.tw>
2025-10-31 02:21:58 +0800

    chore: update dependencies
    - Add Pillow to dependencies for image processing
    - Add sys.path configuration in wsgi.py for deployment
    - Update dependency-groups format in pyproject.toml
    - Update uv.lock
    
    (Synced from z2 remote server changes)

--- exhibition/backend/mysite/wsgi.py
+++ exhibition/backend/mysite/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# 將專案根目錄加入 sys.path
+sys.path.append('/var/www/sites/T-Ambassador-2025/exhibition/backend')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
